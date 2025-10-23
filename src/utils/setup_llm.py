e0f93df67d3d9cf1bf30844442203298a52cf08c
V1SHAL421 <vishalmkumar3@gmail.com>
2025-10-23 20:52:02 +0100

    fix: update deprecated models

--- src/utils/setup_llm.py
+++ src/utils/setup_llm.py
@@ -2,15 +2,15 @@ from langchain_openai import ChatOpenAI
 import streamlit as st
 
 
-def setup_groq_llm(expt_llm="gemma2-9b-it", temperature=0.2):
+def setup_groq_llm(expt_llm="llama-3.1-8b-instant", temperature=0.2):
     assert st.secrets.get("GROQ_API_KEY") is not None, (
         "GROQ_API_KEY is not set. Please set it in .env file"
     )
 
     llm = ChatOpenAI(
-        model_name=expt_llm,
+        model=expt_llm,
         temperature=temperature,
-        max_tokens=1000,
+        max_completion_tokens=1000,
         verbose=True,
         timeout=None,
         api_key=st.secrets.get("GROQ_API_KEY"),
